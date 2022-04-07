bfd53c3c816eb17165c33a9d2c1b0b0ec265e931
Brian Steele <brian.steele@hey.com>
2022-04-07 06:49:47 -0700

    adding files

---
+++ server/src/resolvers.ts
@@ -0,0 +1,16 @@
+export default {
+  Query: {
+    currentUser: () => {
+      return {
+        id: '123',
+        name: 'John Doe',
+        handle: 'johndoe',
+        coverUrl: '',
+        avatarUrl: '',
+        createdAt: '',
+        updatedAt: '',
+      };
+    },
+    suggestions: () => [],
+  },
+};
